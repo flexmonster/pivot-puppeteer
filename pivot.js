83ddb23b5be338de0daf70ef65edcbb0e0bf7010
Masha <marvisnotamused@gmail.com>
2020-11-19 13:18:43 +0200

    Adding comments

--- pivot.js
+++ pivot.js
@@ -5,14 +5,14 @@ const puppeteer = require('puppeteer');
 
 let eventEmitter = new events.EventEmitter();
 
-const directoryPath = "./storage/"; /* Path to save files */
+const directoryPath = "./storage/"; /* A path to the storage of exported files */
 
 ((directoryPath) => {
   fs.mkdir(path.resolve(path.resolve(),
     directoryPath.replace(/^\.*\/|\/?[^\/]+\.[a-z]+|\/$/g, '')), { recursive: true }, error => {
       if (error) console.error(error);
     });
-})(directoryPath); /* Creating directories of the specified path if they are absent */
+})(directoryPath); /* Creating a storage folder for exported files (if such a folder doesn't exist yet) */
 
 (async () => {
 
@@ -39,7 +39,7 @@ const directoryPath = "./storage/"; /* Path to save files */
       Additional methods and events can be added using the template.
     */
 
-    eventEmitter.once('reportcomplete', () => { /* Exporting when report is ready */
+    eventEmitter.once('reportcomplete', () => { /* Exporting when the report is ready */
       exportTo("csv");
       exportTo("html");
       exportTo("pdf");
@@ -50,7 +50,7 @@ const directoryPath = "./storage/"; /* Path to save files */
     let exportCount = 0;
     eventEmitter.on('exportcomplete', () => {
       exportCount++;
-      if (exportCount == 5) browser.close(); /* Closing browser when all exports are completed */
+      if (exportCount == 5) browser.close(); /* Closing the browser when all the exports are complete */
     });
 
     setReport({
@@ -61,15 +61,18 @@ const directoryPath = "./storage/"; /* Path to save files */
 
   });
 
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+  const browser = await puppeteer.launch(); /* Launching the headless browser */
+  const page = await browser.newPage(); /* Creating a new page */
 
+  /* A function to set a report for the component dynamically */
   function setReport(report) {
     page.evaluate(report => {
       flexmonster.setReport(report);
     }, report)
   }
 
+  /* This code is responsible for the export itself. It supports five export formats: 
+     .html, .xlsx, .pdf, .csv, and .png. */
   function exportTo(type, params) {
     page.evaluate((type, params) => {
       type = type.toLowerCase();
@@ -112,6 +115,9 @@ const directoryPath = "./storage/"; /* Path to save files */
     });
   });
 
+
+  /* This code adds functions to emit ready, reportcomplete, and exportcomplete events for the browser 
+     when called. This approach allows us to handle the component's events in the browser's scope. */
   await page.exposeFunction('onReady', () => {
     eventEmitter.emit('ready')
   });
@@ -122,8 +128,11 @@ const directoryPath = "./storage/"; /* Path to save files */
     eventEmitter.emit('exportcomplete')
   });
 
+  /*  Reading the file with the component and setting it as the browser page's contents */
   await page.setContent(fs.readFileSync('index.html', 'utf8'));
 
+  /* This code runs in the page's scope, subscribing the browser window to the component's ready, 
+     reportcomplete, and exportcomplete events */
   await page.evaluate(() => {
     window.addEventListener('ready', () => window.onReady());
     window.addEventListener('reportcomplete', () => window.onReportComplete());
